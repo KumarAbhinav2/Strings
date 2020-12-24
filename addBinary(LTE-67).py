57daaa591e5d7f84c7763d5c5bf65ea1a1103392
Abhinav <er.abhi01@gmail.com>
2020-12-25 00:08:55 +0530

    adds bit manipulation solution'

---
+++ addBinary(LTE-67).py
@@ -0,0 +1,26 @@
+"""
+Given two binary strings a and b, return their sum as a binary string.
+
+
+
+Example 1:
+
+Input: a = "11", b = "1"
+Output: "100"
+Example 2:
+
+Input: a = "1010", b = "1011"
+Output: "10101"
+"""
+
+class Solution:
+    def addBinary(self, a, b):
+        x, y = int(a, 2), int(b, 2)
+        while y:
+            answer = x ^ y
+            carry = (x & y) << 1
+            x, y = answer, carry
+        return bin(x)[2:]
+
+    # Time Complexity: O(N+M), lengths of input strings
+    # Space complexity: O(max(M,N))
